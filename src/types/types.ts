58e565ebf4036fbe7e674f99fc86376a947636bd
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2025-02-12 14:14:35 +0200

    feat(types): Add apartment types

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface Apartment {
+  id: number;
+  unitName: string;
+  unitNumber: string;
+  projectName: string;
+  description?: string;
+  sizeSQM?: number;
+  bathrooms?: number;
+  bedrooms?: number;
+}
