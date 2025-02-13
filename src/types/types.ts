a829a2d66828051028da48d4eaa7c1f66471ee83
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2025-02-13 10:00:23 +0200

    feat(types): Add project type

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,9 +2,17 @@ export interface Apartment {
   id: number;
   unitName: string;
   unitNumber: string;
+  projectName?: string;
+  price: number;
+  description: string;
+  sizeSQM: number;
+  bathrooms: number;
+  bedrooms: number;
+  projectId: number | undefined;
+}
+
+export interface Project {
+  id: number;
   projectName: string;
-  description?: string;
-  sizeSQM?: number;
-  bathrooms?: number;
-  bedrooms?: number;
+  address: string;
 }
